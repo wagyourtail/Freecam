f85f8fe4e0d8d87aa071e4b16e38f58893e55fea
wagyourtail <ruwagray@gmail.com>
2020-06-03 11:30:27 -0600

    comment

--- src/main/java/xyz/wagyourtail/freecam/mixins/WorldRendererMixin.java
+++ src/main/java/xyz/wagyourtail/freecam/mixins/WorldRendererMixin.java
@@ -9,6 +9,8 @@ import xyz.wagyourtail.freecam.Freecam;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
+	
+	//cave culling fix
 	@ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;setupTerrain(Lnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/Frustum;ZIZ)V"), index = 4)
 	public boolean isSpectator(boolean spectator) {
 		return spectator || Freecam.isFreecam;
