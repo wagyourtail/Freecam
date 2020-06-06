3ffa09336a6088765d8a8763d3bb109fa56c21a4
wagyourtail <ruwagray@gmail.com>
2020-06-06 15:30:45 -0600

    cleanup

--- src/main/java/xyz/wagyourtail/freecam/mixins/CameraMixin.java
+++ src/main/java/xyz/wagyourtail/freecam/mixins/CameraMixin.java
@@ -3,9 +3,6 @@ package xyz.wagyourtail.freecam.mixins;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Camera;
